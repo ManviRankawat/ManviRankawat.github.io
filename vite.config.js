dd94db10e179fc09285abba969b2918f616bb710
Manvi Rankawat <81751478+ManviRankawat@users.noreply.github.com>
2025-03-27 21:34:37 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/ManviRankawat.github.io/",
+  base: "/ManviRankawat.github.io",
 });
